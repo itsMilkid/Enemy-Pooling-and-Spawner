109e1b36e41991d2256e79a1d2642b6d12735478
itsMilkid <itsMilkid@gmail.com>
2017-06-14 16:06:09 +0200

    Create SpawnManager.cs

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -27,7 +27,7 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour {
+public class Spawner : MonoBehaviour {
 
 	[Header("General Settings:")]
 	public string poolType;
@@ -44,17 +44,67 @@ public class SpawnManager : MonoBehaviour {
 	[HideInInspector] public enum SPAWNMODE{WAVE,PERMANENT};
 	[HideInInspector] public enum SPAWNSTATE{SPAWNING,WAITING};
 
+	private float delayTimer;
 	private EnemyPooling pooling;
-	private int spawnIndex;
 	private Vector3 currentSpawnpoint;
-
 	private SPAWNSTATE spawnState;
-	private List<GameObject> activeObjects = new List<GameObject>();
+	[HideInInspector] public List<GameObject> activeObjects = new List<GameObject>();
 
 	private void Awake(){
 		activeObjects.Clear();
-		spawnIndex = 0;
+
 		pooling = gameObject.GetComponentInParent<EnemyPooling>();
+
+		currentSpawnpoint.y = transform.position.y;
+		spawnState = SPAWNSTATE.WAITING;
+	}
+
+	private void Update(){
+		//Spawns an enemy as soon as there are less then the set amount
+		//of enemies then there should be in the scene.
+		if(spawnMode == SPAWNMODE.PERMANENT){
+			if(activeObjects.Count < maxInScene){
+				if(spawnState == SPAWNSTATE.WAITING){
+					GenerateSpawnpoint();
+				}
+			}
+		//Respawns enemies in a wave as soon as all enemies are dead/gone
+		} else if (spawnMode == SPAWNMODE.WAVE){
+			if(activeObjects.Count == 0){
+				if(spawnState == SPAWNSTATE.WAITING){
+					GenerateSpawnpoint();
+				}
+			}
+		}
+	}
+
+	//Generates random spawnpoint within a given radius;
+	private void GenerateSpawnpoint(){
+		currentSpawnpoint.x = Random.Range(transform.position.x - spawnRadius,transform.position.x + spawnRadius);
+		currentSpawnpoint.z = Random.Range(transform.position.z - spawnRadius,transform.position.z + spawnRadius);
+		CheckSpawnpoint(currentSpawnpoint);
+	}
+
+	//Checks if anything would be blocking the enemy on that specific waypoint, if
+	//nothing is blocking, we'll proceed to spwan. If there is something in the way
+	//a new spawnpoint will be genereated.
+	private void CheckSpawnpoint(Vector3 _spawnpoint){
+		if(!Physics.CheckSphere(_spawnpoint,objectRadius,unwalkableLayers)){
+			SpawnEnemy(_spawnpoint);
+		} else {
+			GenerateSpawnpoint();
+		}
+	}
+	//Spawns an enemy on the given spawnpoint, removes spawned enemie from the pool
+	//adds enemy to a list of active objects.
+	private void SpawnEnemy(Vector3 _spawnPoint){
+		spawnState = SPAWNSTATE.SPAWNING;
+		GameObject spawnedObject = pooling.poolsDictionairy[poolType][0];
+		spawnedObject.transform.position = _spawnPoint;
+		spawnedObject.SetActive(true);
+		activeObjects.Add(spawnedObject);
+		pooling.poolsDictionairy[poolType].Remove(spawnedObject);
+		spawnState = SPAWNSTATE.WAITING;
 	}
 
 }
